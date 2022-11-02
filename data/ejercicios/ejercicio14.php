af9c9f376e76f42d12c73e29825b48f6b7794aa2
Aitor Gracia <20aitor02@gmail.com>
2022-11-02 16:03:41 +0000

    2/10/2022_2

--- data/ejercicios/ejercicio14.php
+++ data/ejercicios/ejercicio14.php
@@ -41,3 +41,60 @@
     ?>
 </body>
 </html>
+<?php
+        if(isset($_POST['nombre']) && !empty($_POST['nombre']) && strlen($_POST['nombre']) >= 3)
+        {
+            $nombre = $_POST['nombre'];
+
+
+            echo('<body>
+                <h2>Formulario</h2>
+                <form method="post" action="">
+                <label for="nombre">Nombre: </label>
+                <input type="text" value=" " name="nombre" required> <br>
+                <input type="submit" value="enviar">
+                </form>
+            
+                <h2>Datos recibidos</h2>
+                <p> Hola '.$nombre.'</p>
+                </body>');
+        }
+        else if(isset($_POST['nombre']) && !empty($_POST['nombre']) && strlen($_POST['nombre']) < 3)
+        {
+
+            $nombre = $_POST['nombre'];
+
+            echo('<body>
+                <h2>Formulario</h2>
+                <h3>El nombre tiene que tener una longitud mayor a 3</h3>
+                <form method="post" action="">
+                <label for="nombre">Nombre: </label>
+                <input type="text" value="'.$nombre. '" name="nombre" required> <br>
+                <input type="submit" value="enviar">
+                </form>
+            
+                <h2>Datos recibidos</h2>
+                </body>');
+
+
+        }  
+        else
+        {
+            echo('<form action="" method="post">
+            <label for="numero1">Operando 1: </label>
+            <input type="number" value="" name="numero1">
+    
+            <select name="operacion" id="operacion" >
+                <option value="division">÷</option>
+                <option value="multiplicacion">*</option>
+                <option value="resta">-</option>
+                <option value="suma">+</option>
+            </select>
+    
+            <label for="numero2">Operando 2: </label>
+            <input type="number" value="" name="numero2">
+        
+            <input type="submit" value="enviar">
+        </form>');
+        }
+   
